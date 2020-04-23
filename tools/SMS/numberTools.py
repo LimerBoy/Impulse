1a55b88c2db6a628dd4d38ffb96bd5e80e53a392
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2020-04-23 20:27:02 +0200

    fixed indentations

--- tools/SMS/numberTools.py
+++ tools/SMS/numberTools.py
@@ -8,16 +8,15 @@ def normalize(phone):
 
 # Make for other services
 def transformPhone(phone, i):
-	# Pizzahut
-	if i == 5:
-		return '+' + phone[0] + ' (' + phone[1:4] + ') ' + phone[4:7] + ' ' + phone[7:9] + ' ' + phone[9:11] # '+7 (915) 350 99 08'
+    # Pizzahut
+    if i == 5:
+        return '+' + phone[0] + ' (' + phone[1:4] + ') ' + phone[4:7] + ' ' + phone[7:9] + ' ' + phone[9:11] # '+7 (915) 350 99 08'
 
 # Get country name by phone
 def getCountry(phone):
-	try:
-		query = parse("+" + phone, None)
-		country = repr(geocoder.description_for_number(query, 'en'))
-	except:
-		country = "Unknown"
-		
-	return  country
+    try:
+        query = parse("+" + phone, None)
+        country = repr(geocoder.description_for_number(query, 'en'))
+    except:
+        country = "Unknown"
+    return country
